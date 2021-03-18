5fee945784a5ff8d010cc0bec90b3796cf537774
AlexandreSanscartier <alexandre.sanscartier@codefor.ca>
2021-03-18 14:52:59 -0400

    [develop] Testing Pipeline

--- DocumentService/Startup.cs
+++ DocumentService/Startup.cs
@@ -37,12 +37,12 @@ namespace DocumentService
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment())
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DocumentService v1"));
-            }
+            //}
 
             app.UseHttpsRedirection();
 
