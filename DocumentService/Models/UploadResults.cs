c163360a4c6575706e2214dcc697955657d0fb62
TC-Cory <cory.chenier@tc.gc.ca>
2021-11-22 11:45:17 -0500

    [AB#143604-Fix-File-Upload-Performance] Create UploadResults.cs

---
+++ DocumentService/Models/UploadResults.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DocumentService.Models
+{
+    public class UploadResults
+    {
+            public bool Uploaded { get; set; }
+            public string FileName { get; set; }
+            public string StoredFileName { get; set; }
+            public int ErrorCode { get; set; }  
+    }
+}
